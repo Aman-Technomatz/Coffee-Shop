fee0f205f20b1268a5260da9f88d7bf8a431c269
Aman Tomar <atomar@technomatz.com>
2022-12-09 11:54:50 +0530

    calculate grand total in order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,15 @@
 class Order < ApplicationRecord
-  has_many :order_items, inverse_of: :order, dependent: :destroy
+	has_many :order_items, inverse_of: :order, dependent: :destroy
 	accepts_nested_attributes_for :order_items, allow_destroy: true, reject_if: :all_blank
 	has_many :items, through: :order_items, dependent: :destroy
+	after_save :save_grand_total
+
+	# calculate save_grand_total
+	def save_grand_total
+    total = order_items.sum(:total_price)
+		return if grand_total == total
+
+    # total = order_items.sum(:total_price)
+		update(grand_total: total)
+	end
 end
